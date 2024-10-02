4b02a8e8fee0d4ebffd993d6703055ca50ecaea5
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-10-02 15:19:29 +0300

    Add form validators

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -21,9 +21,9 @@ export class UserFormComponent {
   constructor(private formBuilder: FormBuilder) {
     this.userForm = this.formBuilder.group({
       name: ['', Validators.required],
-      email: [''],
-      role: [''],
-      birthdate: [new Date()]
+      email: ['', Validators.email],
+      role: ['', Validators.required],
+      birthdate: [new Date(), Validators.required]
     });
 
     // userForm = new FormGroup({
